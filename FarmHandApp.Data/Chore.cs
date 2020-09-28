f47f91263b9b446729f8979488d70fd9c9bba535
bmagee2 <bamagee2@gmail.com>
2020-09-28 12:38:08 -0400

    Chore, UserChore, Note

--- FarmHandApp.Data/Chore.cs
+++ FarmHandApp.Data/Chore.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +10,62 @@ namespace FarmHandApp.Data
 {
     public class Chore
     {
+        [Key]
+        public int ChoreId { get; set; }
 
+        [ForeignKey(nameof(User))]
+        public string UserId { get; set; }
+        public virtual ApplicationUser User { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public ChoreLocation Location { get; set; }
+
+        [Required]
+        public TypeOfAnimal Animal { get; set; }
+
+        [Required]
+        public TimeOfDay TimeOfDay { get; set; }
+
+        [Required]
+        public bool IsDaily { get; set; }
+
+        public DateTimeOffset CreatedUtc { get; set; }
+
+        public DateTimeOffset ModifiedUtc { get; set; }
+
+        public virtual List<Note> Notes { get; set; }
+    }
+
+    public enum ChoreLocation
+    {
+        Animal_Encounters_Barn,
+        Prairie_Town_Barn,
+        Bank_Barn,
+        Ag_Office,
+        Facilities_Building,
+        Other
+    }
+
+    public enum TypeOfAnimal
+    {
+        Sheep,
+        Goats,
+        Cows,
+        Donkeys,
+        Chickens,
+        Pigs,
+        Other
+    }
+
+    public enum TimeOfDay
+    {
+        Morning,
+        Evening
     }
 }
--- FarmHandApp.Data/Note.cs
+++ FarmHandApp.Data/Note.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +10,22 @@ namespace FarmHandApp.Data
 {
     public class Note
     {
+        [Key]
+        public int NoteId { get; set; }
 
+        [ForeignKey(nameof(User))]
+        public string UserId {get; set;}
+        public virtual ApplicationUser User { get; set; }
+
+        [ForeignKey(nameof(UserChore))]
+        public int UserChoreId { get; set; }
+        public virtual UserChore UserChore { get; set; }
+
+        [Required]
+        public string NoteText { get; set; }
+
+        public DateTimeOffset CreatedUtc { get; set; }
+
+        public DateTimeOffset ModifiedUtc { get; set; }
     }
 }
